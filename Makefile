b7a440b474f122ad9954abf9dfd884c685407868
Camilo Berneri <cberneri@gmail.com>
2023-09-13 18:16:26 +0200

    Add files via upload

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ SOURCES = \
 	ft_isascii.c \
 	ft_isdigit.c \
 	ft_isprint.c \
+	ft_itoa.c \
 	ft_memchr.c \
 	ft_memcmp.c \
 	ft_memcpy.c \
@@ -28,38 +29,38 @@ SOURCES = \
 	ft_strncmp.c \
 	ft_strnstr.c \
 	ft_strrchr.c \
-	ft_substr.c \
+	ft_strtrim.c \
 	ft_tolower.c \
 	ft_toupper.c \
 
-#BONUS_SOURCES = \
-	ft_lstnew.c ft_lstadd_front.c ft_lstsize.c ft_lstlast.c ft_lstadd_back.c \
-	ft_lstdelone.c ft_lstclear.c ft_lstiter.c ft_lstmap.c
+
+#ft_substr.c \
+
+BONUS_SOURCES = \
+	ft_lstadd_front.c \
+	ft_lstlast.c \
+	ft_lstnew.c \
+	ft_lstsize.c \
 	
 OBJECTS = $(SOURCES:.c=.o)
-#BONUS_OBJECTS = $(BONUS_SOURCES:.c=.o)
+BONUS_OBJECTS = $(BONUS_SOURCES:.c=.o)
 CFLAGS = -Wall -Wextra -Werror
 CC = cc
 all: $(NAME)
 $(NAME): $(OBJECTS)
 	$(AR) -r $@ $?
-#bonus: $(OBJECTS) $(BONUS_OBJECTS)
-#	$(AR) -r $(NAME) $?
+bonus: $(OBJECTS) $(BONUS_OBJECTS)
+	$(AR) -r $(NAME) $?
 %.o: %.c
 	$(CC) -c $(CFLAGS) $?
 clean:
-#	rm -f $(OBJECTS) $(BONUS_OBJECTS)
-	rm -f $(OBJECTS)
+	rm -f $(OBJECTS) $(BONUS_OBJECTS)
 	rm -f *.so
-
-
 fclean: clean
 	rm -f $(NAME)
 re: fclean all
+.PHONY: all bonus clean fclean re
 
 so:
 	$(CC) -nostartfiles -fPIC $(CFLAGS) $(SOURCES)
-	cc -nostartfiles -shared -o libft.so $(OBJECTS)
-
-#.PHONY: all bonus clean fclean re
-.PHONY: all clean fclean re
+	cc -nostartfiles -shared -o libft.so $(OBJECTS) $(BONUS_OBJECTS)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: cberneri < cberneri@student.42prague.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/08 17:14:37 by cberneri          #+#    #+#             */
-/*   Updated: 2023/09/11 14:22:28 by cberneri         ###   ########.fr       */
+/*   Updated: 2023/09/13 17:32:04 by cberneri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,12 @@
 #include <unistd.h>
 #include <stdio.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 int			ft_atoi(char *str);
 void		ft_bzero(void *s, size_t n);
 void		*ft_calloc(size_t nmemb, size_t size);
@@ -26,6 +32,10 @@ int			ft_isascii(int c);
 int			ft_isdigit(int digit);
 int			ft_isprint(int c);
 char		*ft_itoa(int n);
+void		ft_lstadd_front(t_list **lst, t_list *new);
+t_list		*ft_lstlast(t_list *lst);
+t_list		*ft_lstnew(void *content);
+int			ft_lstsize(t_list *lst);
 void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
 void		*ft_memcpy(void *dest, const void *src, size_t n);
@@ -46,7 +56,7 @@ char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 char		*ft_strnstr(const char *big, const char *little, size_t len);
 char		*ft_strrchr(const char *str, int c);
-//char		*ft_strtrim(char const *s1, char const *set);
+char		*ft_strtrim(char const *s1, char const *set);
 char		*ft_substr(char const *s, unsigned int start, size_t len);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
